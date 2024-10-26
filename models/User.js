2bd3e50ca5c8da9112ca9dc02182a3c21b61067b
JeongChan2 <jcstar55@gmail.com>
2024-10-26 21:43:15 +0900

    login

--- models/User.js
+++ models/User.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 const userSchema = Schema(
   {
@@ -20,5 +23,12 @@ userSchema.methods.toJSON = function () {
   return obj;
 };
 
-const User = mongoose.model("User",userSchema);
-module.exports = User
+userSchema.methods.generateToken = async function () {
+  const token = await jwt.sign({ _id: this._id }, JWT_SECRET_KEY, {
+    expiresIn: "1d",
+  });
+  return token;
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const router = express.Router();
 const userAPi = require("./user.api")
+const authApi = require("./auth.api")
 
 router.use("/user",userAPi)
+router.use("/auth",authApi)
 
 module.exports = router;
