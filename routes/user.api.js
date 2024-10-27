7ca7baca924d691816182e498a6497ce3d533d61
JeongChan2 <jcstar55@gmail.com>
2024-10-27 20:14:41 +0900

    feat: login session

--- routes/user.api.js
+++ routes/user.api.js
@@ -1,9 +1,11 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
 const userAPi = require("./user.api");
 const userController = require("../controllers/user.controller");
+const authController = require("../controllers/auth.controller");
 
 // 회원가입
-router.post("/",userController.createUser)
+router.post("/", userController.createUser);
+router.get("/me", authController.authenticate, userController.getUser); // token이 valid한 토큰인지, 이 token을 가지고 유저를 찾아서 리턴
 
 module.exports = router;
