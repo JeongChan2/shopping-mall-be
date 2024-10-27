44fdb2479a62ce7e5635d45993ae5e8a9cb6dc81
JeongChan2 <jcstar55@gmail.com>
2024-10-27 20:32:34 +0900

    product regist be

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -27,10 +27,22 @@ authController.authenticate = async (req, res, next) => {
     const tokenString = req.headers.authorization;
     if (!tokenString) throw new Error("Token not found");
     const token = tokenString.replace("Bearer ", "");
-    jwt.verify(token, JWT_SECRET_KEY,(error,payload)=>{
+    jwt.verify(token, JWT_SECRET_KEY, (error, payload) => {
       if (error) throw new Error("invalid token");
       req.userId = payload._id;
     });
+    next();
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+authController.checkAdminPermission = async (req, res, next) => {
+  try {
+    // token
+    const { userId } = req;
+    const user = await User.findById(userId);
+    if (user.level !== "admin") throw new Error("no permission");
     next();
   } catch (error) {
     res.status(400).json({ status: "fail", error: error.message });
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require("express")
 const router = express.Router();
 const userAPi = require("./user.api")
 const authApi = require("./auth.api")
+const productApi = require("./product.api")
 
 router.use("/user",userAPi)
 router.use("/auth",authApi)
+router.use("/product",productApi)
 
 module.exports = router;
