45e52c83eb56bfe80b757aeff844545ec9208f7b
JeongChan2 <jcstar55@gmail.com>
2024-10-30 22:37:18 +0900

    product show

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -28,7 +28,16 @@ productController.createProduct = async (req, res) => {
     });
 
     await product.save();
-    res.status(200).json({status:"success",product})
+    res.status(200).json({ status: "success", product });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+productController.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ status: "success", data: products });
   } catch (error) {
     res.status(400).json({ status: "fail", error: error.message });
   }
