b79e1e87a9ac41790acb503ea7a42cf5885fdd90
JenogChan2 <jcstar55@gmail.com>
2024-11-03 22:11:25 +0900

    cart

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,3 +1,4 @@
+const { populate } = require("dotenv");
 const Cart = require("../models/Cart");
 
 const cartController = {};
@@ -8,7 +9,7 @@ cartController.addItemToCart = async (req, res) => {
     const { productId, size, qty } = req.body;
     // 유저를 가지고 카트 찾기
     let cart = await Cart.findOne({ userId });
-    
+
     if (!cart) {
       // 유저가 만든 카트가 없다, 만들어주기
       cart = new Cart({ userId });
@@ -30,6 +31,60 @@ cartController.addItemToCart = async (req, res) => {
     res
       .status(200)
       .json({ status: "success", data: cart, cartItemQty: cart.items.length });
+  } catch (error) {
+    return res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+cartController.getCart = async (req, res) => {
+  try {
+    const { userId } = req;
+    const cart = await Cart.findOne({ userId }).populate({
+      path:'items',
+      populate:{
+        path: "productId",
+        model:"Product"
+      }
+    });
+    res.status(200).json({ status: "success", data: cart.items });
+  } catch (error) {
+    return res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+cartController.deleteCartItem = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req;
+    const cart = await Cart.findOne({ userId });
+    cart.items = cart.items.filter((item) => !item._id.equals(id));
+
+    await cart.save();
+    res.status(200).json({ status: 200, cartItemQty: cart.items.length });
+  } catch (error) {
+    return res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+cartController.editCartItem = async (req, res) => {
+  try {
+    const { userId } = req;
+    const { id } = req.params;
+
+    const { qty } = req.body;
+    const cart = await Cart.findOne({ userId }).populate({
+      path: "items",
+      populate: {
+        path: "productId",
+        model: "Product",
+      },
+    });
+    if (!cart) throw new Error("There is no cart for this user");
+    const index = cart.items.findIndex((item) => item._id.equals(id));
+    if (index === -1) throw new Error("Can not find item");
+    cart.items[index].qty = qty;
+    await cart.save();
+    res.status(200).json({ status: 200, data: cart.items });
   } catch (error) {
     return res.status(400).json({ status: "fail", error: error.message });
   }
--- routes/cart.api.js
+++ routes/cart.api.js
@@ -3,6 +3,14 @@ const authController = require("../controllers/auth.controller");
 const cartController = require("../controllers/cart.controller");
 const router = express.Router();
 
-router.post("/", authController.authenticate,cartController.addItemToCart);
+router.post("/", authController.authenticate, cartController.addItemToCart);
+router.get("/", authController.authenticate, cartController.getCart);
+router.delete(
+  "/:id",
+  authController.authenticate,
+  cartController.deleteCartItem
+);
+router.put("/:id", authController.authenticate, cartController.editCartItem);
+
 
 module.exports = router;
